604431c2677adf4afeffc8669fee2c62992f4b17
Iker Fernández <148960996+IkerFerCol@users.noreply.github.com>
2025-02-27 13:41:06 +0100

    commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'app-empleados' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-empleados');
+    expect(app.titulo).toEqual('app-empleados');
   });
 
   it('should render title', () => {
